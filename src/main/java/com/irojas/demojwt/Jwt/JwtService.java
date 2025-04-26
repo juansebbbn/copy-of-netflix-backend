c014469d0f0204101161c6096ca774770966acaa
Juan Badone <juansebastianbadone_@hotmail.com>
2025-04-26 15:18:46 -0300

    correciones

--- src/main/java/com/irojas/demojwt/Jwt/JwtService.java
+++ src/main/java/com/irojas/demojwt/Jwt/JwtService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import com.irojas.demojwt.Models.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,13 @@ public class JwtService {
 
     private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
 
-    public String getToken(UserDetails user) {
-        return getToken(new HashMap<>(), user);
+    public String getToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        claims.put("role", user.getRole().name());
+        return getToken(claims, user);
     }
+
 
     private String getToken(Map<String,Object> extraClaims, UserDetails user) {
         return Jwts
